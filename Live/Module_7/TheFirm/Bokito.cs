04029d1ba6e1b1beda4fea4560811dfeb9ca8472
unknown <pschmidt@pasit.nl>
2024-11-26 14:37:04 +0100

    day2

--- Live/Module_7/TheFirm/Bokito.cs
+++ Live/Module_7/TheFirm/Bokito.cs
@@ -2,8 +2,13 @@
 
 
 // Bokito _IMPELENTEERT_ de interface IContract
-internal class Bokito : IContract
+internal class Bokito : IContract, IZoo
 {
+    public void Show()
+    {
+        Console.WriteLine("Bokito slaat op zijn borst");
+    }
+
     public void Werken()
     {
         Console.WriteLine("Bokito ramt dames in elkaar");
--- Live/Module_7/TheFirm/IZoo.cs
+++ Live/Module_7/TheFirm/IZoo.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace TheFirm;
 
-namespace TheFirm
+internal interface IZoo
 {
-    internal interface IZoo
-    {
-    }
+    void Show();
 }
